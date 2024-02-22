96a2a3434cd5d7c58a4a5c9a64d965b5c84b0a20
Jasurbek <jasurbek.saydullayev@list.ru>
2024-02-23 02:33:56 +0500

    updated

--- models.py
+++ models.py
@@ -1,3 +1,20 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+
+
+
+
+
